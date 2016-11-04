e83c2acc62abbd7650f22d90582763df0c36e6b5
João Emmanuel <joaoeizidio@ufrn.edu.br>
2016-11-04 17:28:06 -0300

    Função remove adicionada

--- src/br/imd/modelo/Tree.java
+++ src/br/imd/modelo/Tree.java
@@ -55,9 +55,90 @@ public class Tree {
 			}
 		}
 	}
+	public boolean remove (int matricula) {
+		if (this.root == null) {
+			return false;
+		}
+		if (this.root.getAluno().getMatricula() == matricula) {
+			if (this.leftTree == null && this.rightTree == null) {
+				this.root = null;
+			} else if (this.leftTree != null && this.rightTree == null) {
+				this.root = this.leftTree.root;
+				this.leftTree = this.leftTree.leftTree;
+				this.rightTree = this.leftTree.rightTree;
+			} else if (this.leftTree == null && this.rightTree != null) {
+				this.root = this.rightTree.root;
+				this.leftTree = this.rightTree.leftTree;
+				this.rightTree = this.rightTree.rightTree;
+			} else {
+				Tree ant = this;
+				Tree atual = this.leftTree;
+				while (atual.rightTree != null) {
+					ant = atual;
+					atual = atual.rightTree;
+				}
+				this.root = atual.root;
+				if (ant == this) {
+					this.leftTree = atual.leftTree;
+				} else {
+					ant.rightTree = atual.leftTree;
+				}
+			}
+			return true;
+		}
+		Tree ant = null;
+		Tree atual = this;
+		while (atual != null && atual.root.getAluno().getMatricula() != matricula) {
+			if (atual.root.getAluno().getMatricula() > matricula) {
+				ant = atual;
+				atual = atual.leftTree;
+			} else {
+				ant = atual;
+				atual = atual.rightTree;
+			}
+		}
+		
+		if (atual == null) {
+			return false;
+		}
+		
+		if (atual.leftTree == null && atual.rightTree == null) {
+			if (ant.leftTree == atual) {
+				ant.leftTree = null;
+			} else {
+				ant.rightTree = null;
+			}
+		} else if (atual.leftTree != null && atual.rightTree == null) {
+			if (ant.leftTree == atual) {
+				ant.leftTree = atual.leftTree;
+			} else {
+				ant.rightTree = atual.leftTree;
+			}
+		} else if (atual.leftTree == null && atual.rightTree != null) {
+			if (ant.leftTree == atual) {
+				ant.leftTree = atual.rightTree;
+			} else {
+				ant.rightTree = atual.rightTree;
+			}
+		} else {
+			Tree ant2 = atual;
+			Tree atual2 = atual.leftTree;
+			while (atual2.rightTree != null) {
+				ant2 = atual2;
+				atual2 = atual2.rightTree;
+			}
+			atual.root = atual2.root;
+			if (ant2 == atual) {
+				atual.leftTree = atual2.leftTree;
+			} else {
+				ant2.rightTree = atual2.leftTree;
+			}
+		}
+		return true;
+	}
 	
 	public No busca(int matricula) {
-		// Se n„o encontrar, retorna nÛ vazio
+		// Se n√£o encontrar, retorna n√≥ vazio
 		if (root == null)
 			return new No(new Aluno(0,""));
 		
